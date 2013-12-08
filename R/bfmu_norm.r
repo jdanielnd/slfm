ffcfc834dd8622eaeab5eb06bacce4cd5b280989
João Daniel <jdanielnd@gmail.com>
2013-12-08 16:17:44 -0200

    adding files

---
+++ R/bfmu_norm.r
@@ -0,0 +1,71 @@
+bfmu_r <- function(x, ite=1000, omega=10, omega_1=omega/1000, a=2.1, b=1.1, gamma_a=1, gamma_b=1) {
+
+  alpha <- array(0.1,c(nrow(x),1))
+  sigma2 <- array(1,c(nrow(x),1))
+  z <- array(0,c(nrow(x),1))
+  p_star <- array(0.5,c(nrow(x),1))
+  lambda <- array(0,c(1,ncol(x)))
+  lambda <- t(rnorm(ncol(x)))
+  m <- nrow(x)
+  n <- ncol(x)
+  Dinv = array(0,c(m,m))
+  alpha.matrix <- array(0,c(ite,m))
+  lambda.matrix <- array(0,c(ite,n))
+  sigma.matrix <- array(0,c(ite,m)) 
+  z.matrix <- array(0,c(ite,m))
+  p.matrix <- array(0,c(ite,m))
+
+  tryCatch({
+
+    for(k in 1:ite) {
+      for(i in 1:m) {
+        # sigma sampling
+        A <- a + n/2
+        B <- b + 0.5*( x[i,]%*%x[i,] -2*lambda%*%x[i,]*alpha[i] +alpha[i]*lambda%*%t(lambda)*alpha[i])
+        sigma2[i] <- 1/rgamma(1,A,rate=B) 
+       
+        z[i] <- ifelse(runif(1)<p_star[i],1,0)
+        gamma_1 <- gamma_a + z[i]
+        gamma_2 <- gamma_b + 1 - z[i]
+        p <- rbeta(1, gamma_1, gamma_2)
+        
+        # alpha sampling
+        v_alpha_0 <- 1/((1/omega) + (sum(lambda^2)/sigma2[i]))
+        m_alpha_0 <- v_alpha_0*(sum(x[i,]*lambda)/sigma2[i])
+        v_alpha_1 <- 1/((1/omega_1) + (sum(lambda^2)/sigma2[i]))
+        m_alpha_1 <- v_alpha_1*(sum(x[i,]*lambda)/sigma2[i])
+        
+        if(z[i]) {
+          alpha[i] <- rnorm(1,m_alpha_0,sqrt(v_alpha_0))
+        } else {
+          alpha[i] <- rnorm(1,m_alpha_1,sqrt(v_alpha_1))
+        }
+        frac_1 = dnorm(0,m_alpha_0,sqrt(v_alpha_0),log=T)-dnorm(0,0,sqrt(omega),log=T)
+        frac_2 = dnorm(0,0,sqrt(omega_1),log=T)-dnorm(0,m_alpha_1,sqrt(v_alpha_1),log=T)
+        p_star[i] <- p/(p + exp(frac_1+frac_2)*(1-p))
+        
+        Dinv[i,i] = 1/sigma2[i]
+
+      }
+
+      for(j in 1:ncol(x)) {
+        v_lambda <- 1/(t(alpha)%*%Dinv%*%alpha + 1)
+        m_lambda <- v_lambda*(t(alpha)%*%Dinv%*%x[,j])
+
+        lambda[j] <- rnorm(1,m_lambda,sqrt(v_lambda))
+      }
+
+      alpha.matrix[k,] <- alpha
+      lambda.matrix[k,] <- lambda
+      sigma.matrix[k,] <- sigma2
+      z.matrix[k,] <- z
+      p.matrix[k,] <- p_star
+    }
+    # list(alpha.matrix,lambda.matrix,sigma.matrix,z.matrix,p.matrix)
+    p.matrix
+  },error=function(e) {
+    # list(alpha.matrix,lambda.matrix,sigma.matrix,z.matrix,p.matrix)
+    p.matrix
+  })
+  
+}
---
+++ src/bfm_gibbs.cpp
@@ -0,0 +1,116 @@
+// load Rcpp
+#include <RcppArmadillo.h>
+// [[Rcpp::depends(RcppArmadillo)]]
+
+using namespace Rcpp;       // shorthand
+
+// [[Rcpp::export]]
+NumericMatrix bfmu_c(NumericMatrix x, int ite, double a, double b, double gamma_a,
+    double gamma_b, double omega, double omega_1) {
+
+    int m = x.nrow();
+    int n = x.ncol();
+
+    arma::colvec alpha = rep(0.1, m);
+    arma::rowvec sigma2 = rep(1.0, m);
+    arma::rowvec z = rep(0.0, m);
+    arma::rowvec p_star = rep(0.5, m);
+    arma::rowvec lambda = rnorm(n, 0, 1);
+
+    arma::rowvec debug = rep(0.0, m);
+    arma::mat debug_matrix(ite, m);
+
+    arma::mat dinv(m, m);
+
+    arma::mat alpha_matrix(ite, m);
+    arma::mat lambda_matrix(ite, n);
+    arma::mat sigma_matrix(ite, m);
+    arma::mat z_matrix(ite, m);
+    arma::mat p_matrix(ite, m);
+
+    arma::mat x_arma(x.begin(), m, n, false);
+
+    for (int k = 0; k < ite; k++) {
+        for (int i = 0; i < m; i++) {
+            double A = a + n/2;
+            double B = b + 0.5*(arma::as_scalar(x_arma.row(i)*x_arma.row(i).t()) -
+                2*arma::as_scalar(lambda*x_arma.row(i).t())*alpha[i] + 
+                alpha[i]*arma::as_scalar(lambda*lambda.t())*alpha[i]);
+            sigma2[i] = 1/R::rgamma(A, B);
+
+            if(R::runif(0,1) < p_star[i]) {
+                z[i] = 1;
+            } else {
+                z[i] = 0;
+            }
+
+            double gamma_1 = gamma_a + z[i];
+            double gamma_2 = gamma_b + 1 - z[i];
+            double p = R::rbeta(gamma_1, gamma_2);
+
+            double lambda_ss = 0.0;
+            for(int j = 0; j<n; j++) {
+                lambda_ss += pow(lambda[j], 2);
+            }
+            double v_alpha_0 = 1/(1/omega + lambda_ss/sigma2[i]);
+            double lambda_x_sum = 0.0;
+            for(int j = 0; j<n; j++) {
+                lambda_x_sum += x_arma(i,j)*lambda[j];
+                if (isnan(lambda[j])) {throw std::runtime_error("lambda[j] should not be NaN");}
+                if (isnan(x_arma(i,j))) {throw std::runtime_error("x_arma(i,j) should not be NaN");}
+            }
+            double m_alpha_0 = v_alpha_0*(lambda_x_sum/sigma2[i]);
+            
+            double v_alpha_1 = 1/(1/omega_1 + lambda_ss/sigma2[i]);
+            double m_alpha_1 = v_alpha_1*(lambda_x_sum/sigma2[i]);
+
+            if(z[i]) {
+                alpha[i] = R::rnorm(m_alpha_0, sqrt(v_alpha_0));
+            } else {
+                alpha[i] = R::rnorm(m_alpha_1, sqrt(v_alpha_1));
+            }
+
+            double frac_1 = R::dnorm(0, m_alpha_0, sqrt(v_alpha_0), true) -
+                R::dnorm(0, 0, sqrt(omega), true);
+            double frac_2 = R::dnorm(0, 0, sqrt(omega_1), true) - 
+                R::dnorm(0, m_alpha_1, sqrt(v_alpha_1), true);
+
+            p_star[i] = p/(p + exp(frac_1 + frac_2)*(1-p));
+
+            dinv(i,i) = 1/sigma2[i];
+
+            if (sigma2[i]<0) {throw std::runtime_error("sigma2 should not be negative");}
+            if (isnan(lambda_x_sum)) {throw std::runtime_error("lambda_x_sum should not be NaN");}
+            if (v_alpha_0<0) {throw std::runtime_error("v_alpha_0 should not be negative");}
+            if (v_alpha_1<0) {throw std::runtime_error("v_alpha_1 should not be negative");}
+            if (isnan(m_alpha_0)) {throw std::runtime_error("m_alpha_0 should not be NaN");}
+            if (isnan(m_alpha_1)) {throw std::runtime_error("m_alpha_1 should not be NaN");}
+            if (isnan(sigma2[i])) {throw std::runtime_error("sigma should not be NaN");}
+            if (isnan(alpha[i])) {throw std::runtime_error("alpha should not be NaN");}
+            if (isnan(p_star[i])) {throw std::runtime_error("p_star should not be NaN");}
+            if (isnan(dinv(i,i))) {throw std::runtime_error("dinv should not be NaN");}
+
+            debug[i] = dinv(i,i);
+        }
+
+        for(int j = 0; j<n; j++) {
+            double v_lambda = 1/(arma::as_scalar(alpha.t()*dinv*alpha + 1));
+            double m_lambda = v_lambda*(arma::as_scalar(alpha.t()*dinv*x_arma.col(j)));
+
+            if (v_lambda<0) {throw std::runtime_error("v_lambda cannot be negative");}
+            if (isnan(m_lambda)) {throw std::runtime_error("m_lambda should not be NaN");}
+            if (isnan(lambda[j])) {throw std::runtime_error("lambda should not be NaN");}
+
+            lambda[j] = R::rnorm(m_lambda, sqrt(v_lambda));
+        }
+
+        alpha_matrix.row(k) = alpha;
+        lambda_matrix.row(k) = lambda;
+        sigma_matrix.row(k) = sigma2;
+        z_matrix.row(k) = z;
+        p_matrix.row(k) = p_star;
+        debug_matrix.row(k) = debug;
+
+    }
+    return wrap(debug_matrix);             // Return to R
+}
