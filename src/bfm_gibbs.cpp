d6e6adecb4cdd78464937b6ae71834ecebcec58a
João Daniel <jdanielnd@gmail.com>
2013-12-08 22:42:19 -0200

    reducing intermediate variables

--- src/bfm_gibbs.cpp
+++ src/bfm_gibbs.cpp
@@ -30,11 +30,9 @@ NumericMatrix bfmu_c(NumericMatrix x, int ite, double a = 2.1, double b = 1.1, d
 
     for (int k = 0; k < ite; k++) {
         for (int i = 0; i < m; i++) {
-            double A = a + n/2;
-            double B = b + 0.5*(arma::as_scalar(x_arma.row(i)*x_arma.row(i).t()) -
+            sigma2[i] = 1/R::rgamma(a + n/2, 1/(b + 0.5*(arma::as_scalar(x_arma.row(i)*x_arma.row(i).t()) -
                 2*arma::as_scalar(lambda*x_arma.row(i).t())*alpha[i] + 
-                alpha[i]*arma::as_scalar(lambda*lambda.t())*alpha[i]);
-            sigma2[i] = 1/R::rgamma(A, 1/B);
+                alpha[i]*arma::as_scalar(lambda*lambda.t())*alpha[i])));
 
             if(R::runif(0,1) < p_star[i]) {
                 z[i] = 1;
@@ -42,9 +40,7 @@ NumericMatrix bfmu_c(NumericMatrix x, int ite, double a = 2.1, double b = 1.1, d
                 z[i] = 0;
             }
 
-            double gamma_1 = gamma_a + z[i];
-            double gamma_2 = gamma_b + 1 - z[i];
-            double p = R::rbeta(gamma_1, gamma_2);
+            double p = R::rbeta(gamma_a + z[i], gamma_b + 1 - z[i]);
 
             double lambda_ss = 0.0;
             for(int j = 0; j<n; j++) {
@@ -66,12 +62,9 @@ NumericMatrix bfmu_c(NumericMatrix x, int ite, double a = 2.1, double b = 1.1, d
                 alpha[i] = R::rnorm(m_alpha_1, sqrt(v_alpha_1));
             }
 
-            double frac_1 = R::dnorm(0, m_alpha_0, sqrt(v_alpha_0), true) -
-                R::dnorm(0, 0, sqrt(omega), true);
-            double frac_2 = R::dnorm(0, 0, sqrt(omega_1), true) - 
-                R::dnorm(0, m_alpha_1, sqrt(v_alpha_1), true);
-
-            p_star[i] = p/(p + exp(frac_1 + frac_2)*(1-p));
+            p_star[i] = p/(p + exp(R::dnorm(0, m_alpha_0, sqrt(v_alpha_0), true) -
+                R::dnorm(0, 0, sqrt(omega), true) + R::dnorm(0, 0, sqrt(omega_1), true) - 
+                R::dnorm(0, m_alpha_1, sqrt(v_alpha_1), true))*(1-p));
 
             dinv(i,i) = 1/sigma2[i];
 
@@ -79,9 +72,8 @@ NumericMatrix bfmu_c(NumericMatrix x, int ite, double a = 2.1, double b = 1.1, d
 
         for(int j = 0; j<n; j++) {
             double v_lambda = 1/(arma::as_scalar(alpha.t()*dinv*alpha + 1));
-            double m_lambda = v_lambda*(arma::as_scalar(alpha.t()*dinv*x_arma.col(j)));
-
-            lambda[j] = R::rnorm(m_lambda, sqrt(v_lambda));
+            lambda[j] = R::rnorm(v_lambda*(arma::as_scalar(alpha.t()*dinv*x_arma.col(j))),
+                sqrt(v_lambda));
         }
 
         alpha_matrix.row(k) = alpha.t();
